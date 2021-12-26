12068f389ab621a96d911d50f1278bf33ebd024c
Matthew Todd <Matodd27@gmail.com>
2021-12-26 16:45:47 +0000

    Managed to get the integer value from the prediction output

--- src/tfjs/model.js
+++ src/tfjs/model.js
@@ -6,6 +6,9 @@ async function Model() {
 
     const model = await tf.loadLayersModel("http://localhost:9000/model.json");
     const prediction = model.predict(network_input)
+    const output_array = await prediction.array()
+    const ouptputArgMax = await tf.argMax(output_array, 1).array()
+    console.log(ouptputArgMax)
 }
 
 export default Model;
